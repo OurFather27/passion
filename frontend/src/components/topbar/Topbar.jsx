d5a1745f2c96b3b589279fe35d7a67b55160109b
OurFather27 <148778934+OurFather27@users.noreply.github.com>
2023-10-28 17:43:09 -0400

    Update Topbar.jsx

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -1,58 +1,61 @@
 import "./topbar.css";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext , useState} from "react";
 import { AuthContext } from "../../context/AuthContext";
 import Logout from "../../components/logout/Logout"
-export default function Topbar() {
-  const { user } = useContext(AuthContext);
-  // const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+import { MdEmojiEmotions } from "react-icons/md";
+import { NavLink } from 'react-router-dom'
 
+export default function Topbar() {
+   const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
 
 
   return (
-    <div className="topbarContainer">
-      <div className="topbarLeft">
+    <nav className="topbarContainer">
+      <div className="logo">
         <Link to="/#" style={{ textDecoration: "none" }}>
-          <span className="logo">Glory Church</span>
+          <span className="logo">Church</span>
         </Link>
       </div>
-    
-      <div className="topbarRight">
-        <div className="topbarLinks">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="topbarLink">Home</span>
-          </Link>
-          <Link to="/Minstry"style={{ textDecoration: "none" }}>
-          <span className="topbarLink">Minstrey</span>
-          </Link>
-          <Link to="/Watch"style={{ textDecoration: "none" }}>
-          <span className="topbarLink">Watch</span>
-          </Link>
-          <Link to="/about"style={{ textDecoration: "none" }}>
-          <span className="topbarLink">About us</span>
-          </Link>
-          <Link className="contact"to="/Contact"style={{ textDecoration: "none" }}>
-          <span className="topbarLink">Contact</span>
-          </Link>
-          <Link className="contact"to="/Contact"style={{ textDecoration: "none" }}>
-          <span className="topbarLink">Donate</span>
-          </Link>
-          <Link className="profileLink"to={`/profile/${user.profile}`}style={{ textDecoration: "none", color:"white" }}>
-          {/*<img
-            src={
-              user.profilePicture
-                ? PF + user.profilePicture 
-                : PF + "person/noAvatar.png"
-            }
-            alt=""
-            className="topbarImg"
-          />*/}{user.username}
-        </Link>
-         <Logout/>
+      <div className="menu-icon"onClick={handleShowNavbar}>
+       <MdEmojiEmotions />
+      </div>
+
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+        <div className="NavLinks">
+
+        <ul>
+        <li>
+        <NavLink to="/" style={{ textDecoration: "none" }}>Home</NavLink>
+          </li>
+          <li>
+          <NavLink to="/Minstry"style={{ textDecoration: "none" }}>Minstrey</NavLink>
+          </li>
+          <li>
+          <NavLink to="/Watch"style={{ textDecoration: "none" }}>Watch</NavLink>
+          </li>
+          <li>
+          <NavLink to="/about"style={{ textDecoration: "none" }}>About</NavLink>
+          </li>
+          <li>
+          <NavLink className="contact"to="/Contact"style={{ textDecoration: "none" }}>Contact</NavLink>
+          </li>
+          <li>
+          <NavLink className="contact"to="/Contact"style={{ textDecoration: "none" }}>Donate</NavLink>
+          </li>
+        <li>
+          <Logout/>
+          </li>
+          </ul>
+         
         </div> 
         
 
       </div>
-    </div>
+    </nav>
   );
 }
