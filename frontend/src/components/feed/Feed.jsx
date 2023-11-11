1897712b6299e1b35043476cd46e3c241d03bac1
OurFather27 <148778934+OurFather27@users.noreply.github.com>
2023-11-10 23:36:51 -0500

    Update Feed.jsx

--- frontend/src/components/feed/Feed.jsx
+++ frontend/src/components/feed/Feed.jsx
@@ -4,34 +4,40 @@ import Share from "../share/Share";
 import "./feed.css";
 import axios from "axios";
 import { AuthContext } from "../../context/AuthContext";
+import Watch from "../../pages/watch/Watch"
 
-export default function Feed({ username }) {
+  
+
+
+export default function Feed() {
   const [posts, setPosts] = useState([]);
   const { user } = useContext(AuthContext);
-
+ 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = username
-        ? await axios.get("/posts/profile/" + username)
-        : await axios.get("https://churchbackend.onrender.com/api/posts/timeline/" + user._id);
-      setPosts(
-        res.data.sort((p1, p2) => {
-          return new Date(p2.createdAt) - new Date(p1.createdAt);
+      const res =  await axios.get("https://churchbackend.onrender.com/api/posts");
+      setPosts(res.data.sort((p1, p2) => {
+        return new Date(p2.createdAt) - new Date(p1.createdAt);
         })
       );
     };
     fetchPosts();
-  }, [username, user._id]);
+  }, []);
 
   return (
+    <>
+
     <div className="feed">
-    {(!username || username === user.username) && <Share />}
+   <Share />
       <div className="feedWrapper">
         
-        {posts.map((p) => (
+        {/*{posts.map((p) => (
           <Post key={p._id} post={p} />
-        ))}
+        ))}*/}
       </div>
     </div>
+     
+</>
   );
+
 }
