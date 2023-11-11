93505cc1a8243f8978a6849fc27abbba5f9371b8
OurFather27 <148778934+OurFather27@users.noreply.github.com>
2023-11-10 23:37:32 -0500

    Update Post.jsx

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { format } from "timeago.js";
 import { AuthContext } from "../../context/AuthContext";
 import { Link } from "react-router-dom";
+import data from "./data";
 
 
 export default function Post({ post }) {
@@ -11,68 +12,29 @@ export default function Post({ post }) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   const { user: currentUser } = useContext(AuthContext);
 
-/////////////////////////////////////////////////////
-  var [ViewPost, setModal] = useState(false); // yehegnaw post yehonwon content imge senka view yadergal
-  const ZoomPost =()=>{
-    setModal(!ViewPost)
-
-  }
-///////////////////////////////////////////////////// read more or read less
+////////////////////////////////////////////////////
 var [showFullDescription, setFullDescription] = useState(false);
-
 const showFullDescriptionHandler =()=>{
   setFullDescription(!showFullDescription);
 };
-
 const description = showFullDescription
     ? post.desc
     : post.desc.slice(0, 20);
 /////////////////////////////////////////////////////
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      const res = await axios.get(`https://churchbackend.onrender.com/api/users?userId=${post.userId}`);
-      setUser(res.data);
-    };
-    fetchUser();
-  }, [post.userId]);
-  
   
   return (
   <>
-      <div className="postWrapper">
-        
-
+{/*      <div className="postWrapper">
         <div className="postCenter">
         <span className="postText">{description}</span>
         <button className="btn-readMore" onClick={showFullDescriptionHandler}>
                {showFullDescription ? "...<<" : "...>>"}
           </button>
           <a href={`https://react-icons.github.io/react-icons/icons?name=md`}>
-          <img className="postImg"onClick={ZoomPost}src={PF + post.img}/>
+          <img className="postImg"src={PF + post.img}/>
           </a>
         </div>
-      </div>
-
-
-    {ViewPost && (
-        <div className="modal">
-          <div onClick={ZoomPost} className="overlay"></div>
-          <div className="modal-content">
-      
-        <button className="postCenter">
-        <span className="postText">{post?.desc}</span>
-          
-          <img className="postImg" src={PF + post.img}/>
-        </button>
-            <button className="close-modal" onClick={ZoomPost}>
-              CLOSE
-            </button>
-          </div>
-        </div>
-      )}
-
-
+      </div>*/}
     </>
   );
 }
