42967a4422ce333ca86ec130c3310af5125dde0d
OurFather27 <148778934+OurFather27@users.noreply.github.com>
2023-11-11 00:17:48 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -60,6 +60,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.listen(port, () => {
-  console.log(`Backend server is running! ${port}`);
+app.listen(8800, () => {
+  console.log("Backend server is running!");
 });
+
